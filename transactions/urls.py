6e3384d110906166377ba69b203ae66d3d1d580a
TharHtet236401 <tharhteta3@gmail.com>
2025-05-14 22:58:41 +0700

    Added HTMX support to the transactions page, including a new test endpoint and interactive button for AJAX responses. Updated main.html to include HTMX script and enhanced transactions.html with a test section.

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', login_required(views.transactions), name='transactions'),
+    path('test-htmx/', views.test_htmx, name='test-htmx'),
 ]
 
