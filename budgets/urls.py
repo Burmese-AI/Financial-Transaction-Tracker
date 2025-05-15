937407dfded05db7001f07cd153fd03e938de1ef
TharHtet236401 <tharhteta3@gmail.com>
2025-05-16 02:45:58 +0700

    Implement Budget Creation View and Update URL Routing
    Added a new BudgetCreateView for handling budget creation with user-specific context and success messaging. Updated URL routing to reflect changes in modal paths, enhancing the functionality of the budgets dashboard. Adjusted templates to ensure correct linking for budget creation and improved user feedback through success messages.

--- budgets/urls.py
+++ budgets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BudgetsDashboardView, open_budget_create_modal, open_budget_update_modal
+from .views import BudgetsDashboardView, open_budget_create_modal, open_budget_update_modal, BudgetCreateView
 
 urlpatterns = [
     path('', BudgetsDashboardView.as_view(), name='budgets_dashboard'),
-    path('create/', open_budget_create_modal, name='open_budget_create_modal'),
-    path('update/<int:pk>/', open_budget_update_modal, name='open_budget_update_modal'),
+    path('modal/', open_budget_create_modal, name='open_budget_create_modal'),
+    path('modal/<int:pk>/', open_budget_update_modal, name='open_budget_update_modal'),
+    path('create/', BudgetCreateView.as_view(), name='budget_create'),
 ]
 
--- budgets/views.py
+++ budgets/views.py
@@ -6,6 +6,13 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .forms import BudgetForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.core.paginator import Paginator
+from typing import Any
+from transactions.models import Transaction
+from transactions.forms import TransactionForm
 PAGINATION_SIZE = 10
 # Create your views here.
 class BudgetsDashboardView(ListView):
@@ -39,9 +46,6 @@ class BudgetsDashboardView(ListView):
         context['months'] = [(i, calendar.month_name[i]) for i in range(1, 13)]
         context['years'] = Budget.objects.filter(user=self.request.user).values_list('year', flat=True).distinct().order_by('-year')
         # Add current filter values
-        context['selected_category'] = self.request.GET.get('category', '')
-        context['selected_month'] = self.request.GET.get('month', '')
-        context['selected_year'] = self.request.GET.get('year', '')
         return context
     
     def render_to_response(self, context, **response_kwargs):
@@ -50,6 +54,49 @@ class BudgetsDashboardView(ListView):
             return HttpResponse(html)
         return super().render_to_response(context, **response_kwargs)
   
+
+class BudgetCreateView(CreateView):
+    model = Budget
+    form_class = BudgetForm
+    template_name = "budgets/partials/budgets_table.html"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return self.render_to_response(self.get_context_data())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # To render the whole transaction tabel, paginated transactions are required  
+        budgets = Budget.objects.filter(user=self.request.user)
+        total_budget_limit = sum(budget.amount for budget in budgets)
+        count = budgets.count()
+        paginator = Paginator(budgets, PAGINATION_SIZE)
+        page_obj = paginator.get_page(1)
+        # Merge the existing context dict with the new one
+        context.update({
+            "page_obj": page_obj,
+            "paginator": paginator,
+            "budgets": page_obj.object_list,
+            "is_paginated": paginator.num_pages > 1,
+            "total_budget_limit": total_budget_limit,
+            "count": count,
+        })
+
+        return context
+
+    def render_to_response(self, context: dict[str, Any], **response_kwargs: Any) -> HttpResponse:
+        messages.success(self.request, "Budget added successfully!")
+        if self.request.htmx:
+            # htmx-oob is used to update multiple elements (table and messages) which are not in the same container  
+            context['is_oob'] = True
+            table_html = render_to_string("budgets/partials/budgets_table.html", context, request=self.request)
+            message_html = render_to_string("budgets/components/messages.html", context, request=self.request)
+            return HttpResponse(f"{table_html}{message_html}")
+
+        return super().render_to_response(context)
+
 
 def open_budget_create_modal(request):
     form = BudgetForm()
