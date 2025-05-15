f6b261f629983c840e3c2c5a9bde261ad185ec61
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-05-16 01:08:48 +0700

    Add financial reporting feature

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', login_required(views.transactions), name='transactions'),
+    path('reports/', views.reports_view, name='reports'),
     path('test-htmx/', views.test_htmx, name='test-htmx'),
 ]
 
