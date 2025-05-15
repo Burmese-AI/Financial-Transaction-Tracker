c3f7c0d20aca43a978b13681ee2a470854c6324e
Direwen <direwensik@gmail.com>
2025-05-15 12:59:34 +0630

    Modified transaction modal to be flexible for create/update and added update view

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path("", TransactionListView.as_view(), name="dashboard"),
     path("transactions/", TransactionCreateView.as_view(), name="transaction_create"),
+    path("transactions/<uuid:pk>", TransactionUpdateView.as_view(), name="transaction_update"),
     path("transactions/modal/", open_transaction_create_modal, name="transaction_create_modal"),
+    path("transactions/<uuid:pk>/modal/", open_transaction_update_modal, name="transaction_update_modal"),
     path("close-modal/", close_modal, name="close_modal"),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
 from typing import Any
 from .models import Transaction, Category, Budget
-from .forms import TransactionCreateForm
+from .forms import TransactionForm
 
 PAGINATION_SIZE = 10
 
@@ -30,7 +31,7 @@ class TransactionListView(ListView):
 
 class TransactionCreateView(CreateView):
     model = Transaction
-    form_class = TransactionCreateForm
+    form_class = TransactionForm
     template_name = "partials/transaction_table.html"
 
     def form_valid(self, form):
@@ -57,9 +58,6 @@ class TransactionCreateView(CreateView):
         return context
 
     def render_to_response(self, context: dict[str, Any], **response_kwargs: Any) -> HttpResponse:
-        # Make sure overriden context dict is inlcuded for transactions
-        if context is None: 
-            context = self.get_context_data()
 
         if self.request.htmx:
             # htmx-oob is used to update multiple elements (table and messages) which are not in the same container  
@@ -69,13 +67,49 @@ class TransactionCreateView(CreateView):
             return HttpResponse(f"{table_html}{message_html}")
 
         return super().render_to_response(context)
+ 
+class TransactionUpdateView(UpdateView):
+    model = Transaction
+    form_class = TransactionForm
+    template_name = "partials/transaction_table.html"
+    
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(self.request, "Transaction updated successfully!")
+        return self.render_to_response(self.get_context_data())
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['transaction'] = self.object
+        return context
+    
+    def render_to_response(self, context: dict[str, Any], **response_kwargs: Any) -> HttpResponse:
+        
+        if self.request.htmx:
+            # htmx-oob is used to update multiple elements (table and messages) which are not in the same container  
+            context['is_oob'] = True
+            transaction_html = render_to_string("partials/transaction_row.html", context, request=self.request)
+            message_html = render_to_string("components/messages.html", context, request=self.request)
+            return HttpResponse(f"{transaction_html}{message_html}")
+        return super().render_to_response(context, **response_kwargs)
+     
    
 def open_transaction_create_modal(request):
-    form = TransactionCreateForm()
+    form = TransactionForm()
     context = {
-        'form': form
+        'form': form,
+        'transaction': None,  # Explicitly pass None for create
     }
-    return render(request, "components/transaction_create_modal.html", context=context)
+    return render(request, "components/transaction_modal.html", context=context)
+
+def open_transaction_update_modal(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)  # Ensure user owns the transaction
+    form = TransactionForm(instance=transaction)
+    context = {
+        'form': form,
+        'transaction': transaction,  # Pass the transaction for update
+    }
+    return render(request, "components/transaction_modal.html", context=context)
     
 def close_modal(request):
     return render(request, "components/modal_placeholder.html")
