415ce1975b178abd4e8fa8596082ed1a3d45d45f
TharHtet236401 <tharhteta3@gmail.com>
2025-05-16 10:23:20 +0700

    Update URL patterns to use UUID for budget update and improve modal link in template
    Changed the URL pattern for the budget update modal to accept a UUID instead of an integer for better uniqueness. Updated the budget_row.html template to correctly link to the updated modal path, enhancing user navigation and functionality within the budgets dashboard.

--- budgets/urls.py
+++ budgets/urls.py
@@ -4,7 +4,7 @@ from .views import BudgetsDashboardView, open_budget_create_modal, open_budget_u
 urlpatterns = [
     path('', BudgetsDashboardView.as_view(), name='budgets_dashboard'),
     path('modal/', open_budget_create_modal, name='open_budget_create_modal'),
-    path('modal/<int:pk>/', open_budget_update_modal, name='open_budget_update_modal'),
+    path('modal/<uuid:pk>/', open_budget_update_modal, name='open_budget_update_modal'),
     path('create/', BudgetCreateView.as_view(), name='budget_create'),
     path('delete/<uuid:pk>/', BudgetDeleteView.as_view(), name='budget_delete'),
 ]
