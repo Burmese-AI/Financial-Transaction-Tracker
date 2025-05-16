338110cda22f6a1bb6653ee51585af5cddb111f7
Direwen <direwensik@gmail.com>
2025-05-16 15:45:08 +0630

    Added date range filter, Updated filter dropdowns and combined all searching, filtering and sorting into ListView

--- transactions/urls.py
+++ transactions/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     path("transactions/", TransactionCreateView.as_view(), name="transaction_create"),
     path("transactions/<uuid:pk>/update", TransactionUpdateView.as_view(), name="transaction_update"),
     path("transactions/<uuid:pk>/delete", TransactionDeleteView.as_view(), name="transaction_delete"),
-    path("transactions/search", TransactionSearchView.as_view(), name="transaction_search"),
-    path("transactions/sort", TransactionSortView.as_view(), name="transaction_sort"), 
-    path("transactions/Filter", TransactionFilterView.as_view(), name="transaction_filter"),
-
     ### Function Views
     path("transactions/modal/", open_transaction_create_modal, name="transaction_create_modal"),
     path("transactions/<uuid:pk>/modal/", open_transaction_update_modal, name="transaction_update_modal"),
